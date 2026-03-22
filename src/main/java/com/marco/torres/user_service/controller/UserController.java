d2a827e2811e5530b0784706b717313a84a8ff39
Marco Torres <marco.iturbide99@hotmail.es>
2026-03-22 17:25:18 -0600

    se aplica buenas practicas para manejar correctamente los endpoints.

--- src/main/java/com/marco/torres/user_service/controller/UserController.java
+++ src/main/java/com/marco/torres/user_service/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.marco.torres.user_service.controller;
 
+import java.net.URI;
 import java.util.List;
-
-import org.apache.hc.core5.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.marco.torres.user_service.entity.User;
+import com.marco.torres.user_service.dto.Request;
+import com.marco.torres.user_service.dto.Response;
 import com.marco.torres.user_service.service.UserServiceImpl;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -27,44 +28,48 @@ public class UserController {
     private final UserServiceImpl userService;
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hola desde User Service 📦";
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello from User Service!");
     }
 
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<String> adminOnly() {
-        return ResponseEntity.ok("Admin access granted");
+    public ResponseEntity<String> admin() {
+        return ResponseEntity.ok("Admin access granted!");
     }
 
-    @GetMapping()
-    public List<User> getAll() {
-        return userService.getAll();
+    @GetMapping
+    public ResponseEntity<List<Response>> getAllUser() {
+        return ResponseEntity.ok(userService.getAll());
     }
 
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userService.findUserById(id)
-                .orElseThrow(() -> new RuntimeException("User Not Found"));
+    @GetMapping("/{email}")
+    public ResponseEntity<Response> getUser(@PathVariable String email) {
+        return ResponseEntity.ok(userService.getByUsername(email));
     }
 
     @PostMapping
-    public User create(@RequestBody User user) {
-        return userService.createUser(user);
+    public ResponseEntity<Response> create(@RequestBody @Valid Request request) {
+        Response user = userService.create(request);
+        URI location = URI.create("/users/" + user.getId());
+        return ResponseEntity
+                .created(location)
+                .body(userService.create(request));
+
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
-        return ResponseEntity.ok(userService.updateUser(id, updatedUser));
+    public ResponseEntity<Response> update(@PathVariable Long id, @RequestBody @Valid Request request) {
+        return ResponseEntity.ok(userService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Integer> deleteUser(@PathVariable Long id) {
-        if (userService.deleteUser(id)) {
-            return ResponseEntity.ok(HttpStatus.SC_OK);
-        }
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity
+                .noContent()
+                .build();
     }
 }
